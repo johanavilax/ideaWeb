1e91cfc15b0167b32cf4cba7f4e46f6817bf5ce2
johan <johanavilax@gmail.com>
2020-08-03 19:46:32 -0500

    ajuste mapa

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -5,14 +5,19 @@ import {
     withGoogleMap,
     Marker 
 } from "react-google-maps";
+import mapStyles from "../components/mapStyles";
 
 const Map = ()=>{
     return (
         <GoogleMap 
-            defaultZoom={80}
+            defaultZoom={20}
             defaultCenter={{lat:4.711378, lng: -74.202973}}
+            defaultOptions={{styles:mapStyles,fullscreenControl: false}}
+            
         >
-            <Marker position={{lat:4.711378, lng: -74.202973}}/>
+            <Marker position={{lat:4.711378, lng: -74.202973}}
+            title="IDEA SAS"
+            />
             </GoogleMap>
     );
 };
---
+++ src/components/mapStyles.tsx
@@ -0,0 +1,46 @@
+export default [
+    {
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#273e48"
+        }
+      ]
+    },
+    {
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        },
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "color": "#2da4d7"
+        },
+        {
+          "saturation": -5
+        }
+      ]
+    },
+    {
+      "elementType": "labels.text",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        },
+        {
+          "lightness": 5
+        },
+        {
+          "visibility": "simplified"
+        }
+      ]
+    }
+  ]
