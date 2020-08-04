ab527090d9650f3543526b30754e32ef7d44ebed
johan <johanavilax@gmail.com>
2020-08-03 20:09:00 -0500

    dominio+

--- src/pages/contacto.tsx
+++ src/pages/contacto.tsx
@@ -11,9 +11,22 @@ const Contacto = (props) => {
   <div id="contacto">
     <div className="mainContacto">
       <div className="informacion">
+        <h1>Contacto</h1>
 
+        <h1>Correo electrónico:</h1>
+        <p>Gerencia@ideautomation.com.co</p>
+        <h1>Teléfono</h1>
+        +57 305 766 6868
+        <p></p>
+        <h1>Dirección:</h1>
+        <p>Cra 6 # 19-18 Funza Cundinamarca</p>
+        <h1>Redes:</h1>
+        <FontAwesomeIcon icon={faFacebookF} />
+        <FontAwesomeIcon icon={faInstagram} />
+        <FontAwesomeIcon icon={faLinkedinIn} />
+        <FontAwesomeIcon icon={faWhatsapp} />
       </div>
-      
+      <img src="../automatizacionP.jpg" alt=""/>
     </div>
     <div className="mapa">
         <Map googleMapURL= {"https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyAr56-0s8UbtTUlMrQgHK9DQXmDjcwg4Ss"}
